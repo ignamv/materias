098a93ff8aea30b636f4e0cf790a14d4f407ce02
Ignacio Martinez V <ignamv@gmail.com>
2010-09-26 11:01:37 -0300

    Ayuda, excluir aprobadas en --list, explicitar falta de correlativas
    Agregué ayuda para las opciones de línea de comandos.
    Ahora la lista de materias reconoce el argumento --exclude y no muestra
    las que se aprobaron.
    Cuando se piden las correlativas de una materia, si fueron todas
    aprobadas o si no tiene se escribe "Ninguna".

--- prueba.py
+++ prueba.py
@@ -7,27 +7,24 @@ import argparse
 
 parser = argparse.ArgumentParser(
 		description='Información de materias y correlativas')
-parser.add_argument('-f','--file',default='materias.csv')
-parser.add_argument('-p','--passed-file',dest='pfile',default='aprobadas.txt')
-parser.add_argument('-l','--list', action='store_true',default=False)
-parser.add_argument('-a','--available', action='store_true',default=False)
+parser.add_argument('-f','--file',default='materias.csv',
+		help='Carga la lista de materias de otro archivo')
+parser.add_argument('-p','--passed-file',dest='pfile',default='aprobadas.txt',
+		help='Carga la lista de materias aprobadas de otro archivo')
+parser.add_argument('-l','--list', action='store_true',default=False,
+		help='Mostrar la lista de materias')
+parser.add_argument('-a','--available', action='store_true',default=False,
+		help='Mostrar las materias que puedo cursar')
 parser.add_argument('-x','--exclude-passed', action='store_true',default=False,
-		dest='exclude')
-parser.add_argument('materia', nargs='*', default=[])
+		dest='exclude', help='No mostrar materias ya aprobadas')
+parser.add_argument('materia', nargs='*', default=[],
+		help='Buscar correlativas de estas materias')
 opciones = parser.parse_args()
 
 m = materias(opciones.file)
 
-if opciones.list:
-	print "Lista de materias:"
-	for n in m.mats:
-		mat = m.mats[n]
-		print "%2d %s\t%s" % (n,mat.nombre.ljust(27),"/".join(mat.apodo))
-	sys.exit()
-
 # Cargo la lista de aprobadas
-f = file(opciones.pfile,'r')
-for linea in f:
+for linea in file(opciones.pfile,'r'):
 	linea = linea.strip()
 	if not linea:
 		continue
@@ -39,6 +36,18 @@ for linea in f:
 		print type(linea)
 		continue
 	materia.aprobada = True
+
+if opciones.list:
+	if opciones.exclude:
+		print "Lista de materias por aprobar:"
+	else:
+		print "Lista de materias:"
+	for n in m.mats:
+		mat = m.mats[n]
+		if mat.aprobada and opciones.exclude:
+			continue
+		print "%2d %s\t%s" % (n,mat.nombre.ljust(27),"/".join(mat.apodo))
+	sys.exit()
 
 if opciones.available:
 	# Muestro todas las que puedo cursar
@@ -60,5 +69,9 @@ for busqueda in opciones.materia:
 		print "(excluyendo aprobadas)"
 	else:
 		print
-	for c in m.correlativas(materia,not opciones.exclude):
+	correlativas = m.correlativas(materia,not opciones.exclude)
+	if not correlativas:
+		print "Ninguna"
+		continue
+	for c in correlativas:
 		print c.nombre
